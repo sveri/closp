e1a5f1728930f45ffe79ca83841e4feb3f03519a
sveri <sveri@sveri.de>
2015-02-10 16:58:08 +0100

    adding description

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject closp/lein-template "0.1.1"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "A webframework combining several technologies based on luminus and chestnut."
+  :url "https://github.com/sveri/closp"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [
