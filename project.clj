60c05993e2f163ddb7d4904f5a78581b97ac5103
sveri <sveri@sveri.de>
2015-06-08 21:50:31 +0200

    bugfix "reset"

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject closp/lein-template "0.1.16-Snapshot"
+(defproject closp/lein-template "0.1.16"
   :description "A webframework combining several technologies based on luminus and chestnut."
   :url "https://github.com/sveri/closp"
   :license {:name "Eclipse Public License"
