dda13809bf13a8e4e6285fdb3ed8d88680be3c5d
sveri <sveri80@gmail.com>
2015-07-24 13:01:43 +0200

    removing wrong log message on server start

--- resources/leiningen/new/closp/clj/core.clj
+++ resources/leiningen/new/closp/clj/core.clj
@@ -5,14 +5,8 @@
             [{{ns}}.components.components :refer [prod-system]])
   (:gen-class))
 
-(defn parse-port [args]
-  (if-let [port (->> args (remove #{"-dev"}) first)]
-    (Integer/parseInt port)
-    3000))
-
 (defn -main [& args]
-  (let [port (parse-port args)]
-    (reloaded.repl/set-init! prod-system)
-    (go)
-    (cljc/foo-cljc "hello from cljx")
-    (timbre/info "server started on port:" port)))
+  (reloaded.repl/set-init! prod-system)
+  (go)
+  (cljc/foo-cljc "hello from cljx")
+  (timbre/info "server started."))
