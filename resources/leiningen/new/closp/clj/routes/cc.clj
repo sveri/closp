97fc0cc0a29417267af5283e056fbbbc0e7ae2f7
sveri <sveri80@gmail.com>
2015-09-29 12:17:34 +0200

    adding fields works

--- resources/leiningen/new/closp/clj/routes/cc.clj
+++ resources/leiningen/new/closp/clj/routes/cc.clj
@@ -6,10 +6,15 @@
 (defn cc-page []
   (layout/render "cc/index.html"))
 
-(defn initial-data []
-  (response {:ok "fooo" :loaded true}))
+(defn initial-data [config]
+  (let [files (->> (:closp-definitions config)
+                   (File.)
+                   (file-seq)
+                   (filter #(.isFile %)))
+        files-beautified (map (fn [f] {:name (.getName f) :content (slurp f)}) files)]
+    (response {:ex-entities files-beautified :loaded true})))
 
-(defn cc-routes []
+(defn cc-routes [config]
   (routes
     (GET "/admin/cc" [] (cc-page))
-    (GET "/admin/cc/initial" [] (initial-data))))
+    (GET "/admin/cc/initial" [] (initial-data config))))
--- resources/leiningen/new/closp/cljs/cc/core.cljs
+++ resources/leiningen/new/closp/cljs/cc/core.cljs
@@ -5,19 +5,19 @@
 
 
 ;{:ex-entities []
-; :new-entity {:cur-id Int :name String :fields [{:name "" :type "" :nullable false :id 1}]}}
+; :new-entity {:cur-id Int :name String :fields {1 {:name "" :type "" :nullable false :id 1}
+;                                                2 {:name "" :type "" :nullable false :id 1}}}}
 (def state (atom {}))
 
-
-(def new-entity-definition {:cur-id 1 :name "" :fields {1 {:name "" :type "" :nullable false :id 1}}})
-;(def new-entity-definition {:cur-id 1 :name "" :fields [{:name "" :type "" :nullable false :id 1}]})
+(def new-entity-definition {:cur-id 1 :name "" :fields {1 {:name "" :type "Varchar" :nullable false :id 1}}})
 
 (defn initialized? []
-      (nil? (get @state :ex-entities)))
+      (not (nil? (get @state :ex-entities))))
 
 (defn render-ex-entities [ents]
       [:div
        [:h3 "Existing Entities"]
+       [:hr]
        [:ul
         (for [e ents]
              ^{:key e} [:li {:title (:content e)} (:name e)])]])
@@ -44,19 +44,31 @@
            [:div
             ;[:input {:type "hidden" :value id}]
             [:div.row
-             [:div.col-md-4 (wrap-with-form "Field Name"
-                                            [:input.form-control
-                                             {:on-change   #(println "changed")
-                                              :placeholder "Field Name" :value (:name field)}])]
-             [:div.col-md-4 (wrap-with-form "Field Type" [:select.form-control
-                                                          [:option "Varchar"]
-                                                          [:option "Text"]
-                                                          [:option "Boolean"]])]
-             [:div.col-md-4 (wrap-with-form "Nullable" [:input.form-control {:type "checkbox"}])]]]))
+             [:div.col-md-4
+              (wrap-with-form
+                "Field Name"
+                [:input.form-control
+                 {:on-change   #(swap! state assoc-in [:new-entity :fields field-id :name] (-> % .-target .-value))
+
+                  :placeholder "Field Name" :value (:name field)}])]
+             [:div.col-md-4
+              (wrap-with-form "Field Type"
+                              [:select.form-control
+                               {:on-change #(swap! state assoc-in [:new-entity :fields field-id :type] (-> % .-target .-value))
+                                :value     (get-in @state [:new-entity :fields field-id :type])}
+                               [:option "Varchar"]
+                               [:option "Text"]
+                               [:option "Boolean"]])]
+             [:div.col-md-4
+              (wrap-with-form "Nullable"
+                              [:input.form-control
+                               {:type      "checkbox"
+                                :checked   (get-in @state [:new-entity :fields field-id :nullable])
+                                :on-change #(swap! state assoc-in [:new-entity :fields field-id :nullable]
+                                                   (-> % .-target .-value))}])]]]))
 
 (defn middle-panel []
-      (let [new-fields (get-in @state [:new-entity :fields])
-            fields-count (get-in @state [:new-entity :cur-id])]
+      (let [fields-count (get-in @state [:new-entity :cur-id])]
            [:div
             (wrap-with-form "Entity Name"
                             [:input.form-control {:placeholder "Entity Name"
@@ -70,19 +82,18 @@
             [:hr]
             [:button.btn.btn-default
              {:on-click #(do
-                          (swap! state update-in [:new-entity :fields] conj
-                                 {:name "" :type "" :nullable false :id (+ 1 (get-in @state [:new-entity :cur-id]))})
-                          (swap! state update-in [:new-entity :cur-id] + 1))}
+                          (swap! state update-in [:new-entity :cur-id] + 1)
+                          (swap! state update-in [:new-entity :fields] assoc (get-in @state [:new-entity :cur-id])
+                                 {:name "" :type "" :nullable false :id (get-in @state [:new-entity :cur-id])}))}
              "Add Field"]
             [:button.btn.btn-primary.pull-right "Save"]]))
 
 (defn page []
-      (let [loaded (:ex @state)]
-           (if (initialized?)
-             [:div.row
-              [:div.col-md-3 [left-panel]]
-              [:div.col-md-8 [middle-panel]]]
-             [:div "Loading"])))
+      (if (initialized?)
+        [:div.row
+         [:div.col-md-3 [left-panel (:existing-entities @state)]]
+         [:div.col-md-8 [middle-panel]]]
+        [:div "Loading"]))
 
 (defn init-state []
       (swap! state assoc :new-entity new-entity-definition)
