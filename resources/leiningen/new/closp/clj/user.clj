b481912879710e8292fd8f41cba53ac76d0cd6e3
sveri <sveri80@gmail.com>
2015-12-09 10:56:32 +0100

    enable schema validation on backend in dev env

--- resources/leiningen/new/closp/clj/user.clj
+++ resources/leiningen/new/closp/clj/user.clj
@@ -1,8 +1,10 @@
 (ns {{ns}}.user
   (:require [reloaded.repl :refer [go reset stop]]
+            [schema.core :as s]
             [{{ns}}.components.components :refer [dev-system]]))
 
 (defn start-dev-system []
+  (s/set-fn-validation! true)
   (go))
 
 (reloaded.repl/set-init! dev-system)
