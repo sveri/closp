fa77468f7dcb359f040e88399a3435e28d0b6949
sveri <sveri80@gmail.com>
2015-12-11 22:20:50 +0100

    releasing 0.1.25

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject closp/lein-template "0.1.25-Snapshot"
+(defproject closp/lein-template "0.1.25"
   :description "A webframework combining several technologies based on luminus and chestnut."
   :url "https://github.com/sveri/closp"
   :license {:name "Eclipse Public License"
