e285033068e13c0fa089d962ad5da0c16ce5654f
sveri <sveri@sveri.de>
2017-01-31 15:04:05 +0100

    fixing startup from main

--- resources/leiningen/new/closp/clj/core.clj
+++ resources/leiningen/new/closp/clj/core.clj
@@ -1,10 +1,12 @@
 (ns {{ns}}.core
   (:require [clojure.tools.logging :as log]
+            [system.repl :refer [set-init! start]]
             [{{ns}}.cljccore :as cljc]
             [{{ns}}.components.components :refer [prod-system]]
             [com.stuartsierra.component :as component])
   (:gen-class))
 
 (defn -main [& args]
-  (alter-var-root #'prod-system component/start)
+  (set-init! #'prod-system)
+  (start)
   (log/info "server started."))
