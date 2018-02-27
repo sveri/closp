2612f68784a9f98a9308c5c3045afbba272f54dc
sveri <sveri80@gmail.com>
2018-02-27 16:26:51 +0100

    circleci tests

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject closp/lein-template "0.3.3"
+(defproject closp/lein-template "0.3.4-Snapshot-1"
   :description "An opionated webframework for clojure and clojurescript."
   :url "https://github.com/sveri/closp"
   :license {:name "Eclipse Public License"
