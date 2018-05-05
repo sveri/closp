9c5d32e8d9dd639c2b81337425f9e675a73c292b
sveri <sveri80@gmail.com>
2018-05-05 21:54:12 +0200

    fixing server

--- resources/leiningen/new/closp/clj/components/server.clj
+++ resources/leiningen/new/closp/clj/components/server.clj
@@ -1,29 +1,6 @@
 (ns {{ns}}.components.server
-  (:require [clojure.tools.logging :as log]
-            [immutant.web :as web]
-            [hara.io.scheduler :as sched]
-            [{{ns}}.session :as session]
-            [com.stuartsierra.component :as component])
-  (:import (clojure.lang AFunction)))
-
-(defn destroy
-  "destroy will be called when your application
-   shuts down, put any clean up code here"
-  []
-  (log/info "{{name}} is shutting down...")
-  (sched/shutdown! session/cleanup-job)
-  (log/info "shutdown complete!"))
-
-(defn init
-  "init will be called once when
-   app is deployed as a servlet on
-   an app server such as Tomcat
-   put any initialization code here"
-  [config]
-  ;;start the expired session cleanup job
-  (sched/start! session/cleanup-job)
-  (log/info "\n-=[ {{name}} started successfully"
-               (when (= (:env config) :dev) "using the development profile") "]=-"))
+  (:require [immutant.web :as web]
+            [com.stuartsierra.component :as component]))
 
 (defrecord WebServer [handler config]
   component/Lifecycle
