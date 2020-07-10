8912cbc344bf327f80938379ec2db8f561b82431
sveri <sveri80@gmail.com>
2020-07-10 07:48:12 +0200

    added jvm exception handler and shutdown hook
    removed c3p0

--- resources/clj/new/closp/src/clj/core.clj
+++ resources/clj/new/closp/src/clj/core.clj
@@ -1,7 +1,7 @@
 (ns {{namespace}}.core
   (:require [{{namespace}}.components.components :refer [prod-system]]
             [clojure.tools.logging :as log]
-            [system.repl :refer [set-init! start]])
+            [system.repl :refer [set-init! start stop]])
   (:gen-class))
 
 (defn -main [& args]
@@ -10,6 +10,7 @@
   (log/info "server started."))
 
 (defn shutdown []
+  (log/info "Shutdown hook called, shutting down server")
   (stop))
 
 (.addShutdownHook (Runtime/getRuntime)
